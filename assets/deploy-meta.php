2bbb99b12c3d2602d27e6f756b6bac428dc47191
Antonio Strignano <a.strignano@yahoo.it>
2026-04-29 11:43:14 +0200

    Revert "Fix compatibilità PHP < 8 con polyfill string helpers"
    This reverts commit 0b90c23e76da1690c738dc637c96131801c0e766.

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Fix compatibilità PHP < 8 con polyfill string helpers',
-    'commit_hash'  => '835a19e3',
-    'deployed_at'  => '2026-04-29 11:31:18',
+    'commit_title' => 'Revert "Fix compatibilità PHP < 8 con polyfill string helpers"',
+    'commit_hash'  => '60e99662',
+    'deployed_at'  => '2026-04-29 11:43:58',
 ];
