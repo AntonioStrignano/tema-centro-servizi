9588b207da4cda7bd2c94ed000ea6985f75c9218
Antonio Strignano <a.strignano@yahoo.it>
2026-05-11 11:00:27 +0200

    fix wisdth pag legali

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'data ultima modifica',
-    'commit_hash'  => '0a786257',
-    'deployed_at'  => '2026-05-11 10:56:57',
+    'commit_title' => 'fix wisdth pag legali',
+    'commit_hash'  => 'b7034389',
+    'deployed_at'  => '2026-05-11 11:01:10',
 ];
