345a426cc7af721b7c336357ea3c384ecca9d8a5
Antonio Strignano <a.strignano@yahoo.it>
2026-05-11 10:56:14 +0200

    data ultima modifica

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'ASPETTO PAGINE ISTITUZIONALI',
-    'commit_hash'  => '0144f839',
-    'deployed_at'  => '2026-05-11 10:47:04',
+    'commit_title' => 'data ultima modifica',
+    'commit_hash'  => '0a786257',
+    'deployed_at'  => '2026-05-11 10:56:57',
 ];
