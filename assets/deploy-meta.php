a0693e18d9775cb817cf27eb717315c70576f066
Antonio Strignano <a.strignano@yahoo.it>
2026-05-14 12:55:55 +0200

    Aggiungi reset di margini e sfondi per sezioni della home e padding per contenuti interni

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'boh',
-    'commit_hash'  => 'f931c9e46',
-    'deployed_at'  => '2026-05-14 12:44:16',
+    'commit_title' => 'Aggiungi reset di margini e sfondi per sezioni della home e padding per contenuti interni',
+    'commit_hash'  => '57dca1315',
+    'deployed_at'  => '2026-05-14 12:56:35',
 ];
