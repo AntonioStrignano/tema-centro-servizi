e5b1dd1f93c9bab57d0e711a0793bd8543f5e706
Antonio Strignano <a.strignano@yahoo.it>
2026-05-14 12:43:22 +0200

    boh

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Refactor homepage sections to use new shape dividers and improve layout',
-    'commit_hash'  => 'bf342eb9a',
-    'deployed_at'  => '2026-05-14 12:39:09',
+    'commit_title' => 'boh',
+    'commit_hash'  => 'f931c9e46',
+    'deployed_at'  => '2026-05-14 12:44:16',
 ];
