51af7ccff1fae161d80f8b32267535540efbb5fc
Antonio Strignano <a.strignano@yahoo.it>
2026-05-18 11:59:50 +0200

    Ottimizza gli stili del modulo di ricerca nel filtro trasparenza, riducendo padding e gap

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'bug 2',
-    'commit_hash'  => '6be013a1b',
-    'deployed_at'  => '2026-05-18 11:53:03',
+    'commit_title' => 'Ottimizza gli stili del modulo di ricerca nel filtro trasparenza, riducendo padding e gap',
+    'commit_hash'  => '4bfed5379',
+    'deployed_at'  => '2026-05-18 12:00:31',
 ];
