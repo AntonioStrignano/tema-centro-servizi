cce17d5d2e68832c93c1dd5f8f8efcaf6d4941d6
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 11:00:27 +0200

    fix debug

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'pulizia debug',
-    'commit_hash'  => 'd84536dd9',
-    'deployed_at'  => '2026-05-20 10:39:57',
+    'commit_title' => 'fix debug',
+    'commit_hash'  => '772616585',
+    'deployed_at'  => '2026-05-20 11:01:12',
 ];
