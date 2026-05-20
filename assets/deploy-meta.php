15d09fc073dd2c678ddfc1cb75fca12ed65127db
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 11:14:50 +0200

    fix commit

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'fix css head',
-    'commit_hash'  => '4f037ebcc',
-    'deployed_at'  => '2026-05-20 11:10:09',
+    'commit_title' => 'fix commit',
+    'commit_hash'  => '6bd14f575',
+    'deployed_at'  => '2026-05-20 11:14:51',
 ];
