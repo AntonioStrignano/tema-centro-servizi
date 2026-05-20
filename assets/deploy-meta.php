4f3b0693420c09a28aff76423d3475ecdd648cbf
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 15:52:59 +0200

    fix hero 2

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'fix hero',
-    'commit_hash'  => '3cde1ef06',
-    'deployed_at'  => '2026-05-20 15:51:58',
+    'commit_title' => 'fix hero 2',
+    'commit_hash'  => '9e72dfdc5',
+    'deployed_at'  => '2026-05-20 15:52:59',
 ];
