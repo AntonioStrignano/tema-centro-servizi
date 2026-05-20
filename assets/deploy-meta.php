ef2a4552fa9c4d6663a0adf27ffd22227e651dcc
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 12:48:50 +0200

    modifica l'altezza del pulsante e aggiorna il padding per la sezione home-vitrine

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'modifica la posizione del pulsante e aggiungi padding dinamico per la sezione home-vitrine',
-    'commit_hash'  => '0ce0a2d08',
-    'deployed_at'  => '2026-05-20 12:45:42',
+    'commit_title' => 'modifica l'altezza del pulsante e aggiorna il padding per la sezione home-vitrine',
+    'commit_hash'  => '90036c602',
+    'deployed_at'  => '2026-05-20 12:48:50',
 ];
