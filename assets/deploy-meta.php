ba5a6624f95440d514c8ecf089fd76bff8b47426
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 12:45:41 +0200

    modifica la posizione del pulsante e aggiungi padding dinamico per la sezione home-vitrine

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'aggiungi z-index e overflow per migliorare la gestione della sezione home-vitrine',
-    'commit_hash'  => '552d12625',
-    'deployed_at'  => '2026-05-20 12:39:18',
+    'commit_title' => 'modifica la posizione del pulsante e aggiungi padding dinamico per la sezione home-vitrine',
+    'commit_hash'  => '0ce0a2d08',
+    'deployed_at'  => '2026-05-20 12:45:42',
 ];
