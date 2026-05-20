accd1edac90edce2c5bd931f9ead5eddb84ee2d1
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 12:39:18 +0200

    aggiungi z-index e overflow per migliorare la gestione della sezione home-vitrine

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'invert button mask and adjust positioning for home-vitrine sections',
-    'commit_hash'  => '30ffa432d',
-    'deployed_at'  => '2026-05-20 12:35:44',
+    'commit_title' => 'aggiungi z-index e overflow per migliorare la gestione della sezione home-vitrine',
+    'commit_hash'  => '552d12625',
+    'deployed_at'  => '2026-05-20 12:39:18',
 ];
