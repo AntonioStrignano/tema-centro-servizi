4435ca1ed0f472584846f9d27e05cf80dbb8cee0
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 12:35:44 +0200

    invert button mask and adjust positioning for home-vitrine sections

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'modifica la logica di schiarimento dei colori e aggiorna i valori di luminosità per i colori principali',
-    'commit_hash'  => '5ca1a798f',
-    'deployed_at'  => '2026-05-20 12:33:07',
+    'commit_title' => 'invert button mask and adjust positioning for home-vitrine sections',
+    'commit_hash'  => '30ffa432d',
+    'deployed_at'  => '2026-05-20 12:35:44',
 ];
