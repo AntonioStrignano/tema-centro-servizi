3ffcb3bd594f9802bc3e681bc4204eb007fc5699
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 11:05:17 +0200

    vediamo mo

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'alleluja?',
-    'commit_hash'  => '20ae7e673',
-    'deployed_at'  => '2026-05-20 11:04:54',
+    'commit_title' => 'vediamo mo',
+    'commit_hash'  => 'b040d59ed',
+    'deployed_at'  => '2026-05-20 11:06:09',
 ];
