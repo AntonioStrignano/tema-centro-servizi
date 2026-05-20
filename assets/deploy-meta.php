a9dee04c80fd683016fd5b0831956eab6d48883f
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 10:04:26 +0200

    fix homepage

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'homepage 2',
-    'commit_hash'  => 'a8539c6d0',
-    'deployed_at'  => '2026-05-20 10:00:47',
+    'commit_title' => 'fix homepage',
+    'commit_hash'  => 'c94c47a45',
+    'deployed_at'  => '2026-05-20 10:05:16',
 ];
