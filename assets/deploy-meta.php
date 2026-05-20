87ad467b96e635e931951ddbd2de1814b5ff0017
Antonio Strignano <a.strignano@yahoo.it>
2026-05-20 15:49:15 +0200

    sostituzione ::before

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'ottimizza la gestione delle maschere per i pulsanti e aggiunge una funzione per il parsing dei metadati di distribuzione',
-    'commit_hash'  => '9fd24cc29',
-    'deployed_at'  => '2026-05-20 15:48:12',
+    'commit_title' => 'sostituzione ::before',
+    'commit_hash'  => 'f60293f50',
+    'deployed_at'  => '2026-05-20 15:49:15',
 ];
