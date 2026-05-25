0bbff20732c004fc669072641361a0b3b6d66522
Antonio Strignano <a.strignano@yahoo.it>
2026-05-25 10:00:38 +0200

    Correggi l'ordine delle chiamate a wp_head nel file header.php

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'fix pag legali',
-    'commit_hash'  => 'b91908f38',
-    'deployed_at'  => '2026-05-25 09:57:44',
+    'commit_title' => 'Correggi l'ordine delle chiamate a wp_head nel file header.php',
+    'commit_hash'  => '6d79b18b4',
+    'deployed_at'  => '2026-05-25 10:00:38',
 ];
