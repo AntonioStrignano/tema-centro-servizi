eef99eca79d9e26a26647d75102a45b4147744be
Antonio Strignano <a.strignano@yahoo.it>
2026-05-26 12:26:49 +0200

    logo a dx mobile

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Aggiunge il toggle del menu nella testata del sito e gestisce il comportamento del menu mobile',
-    'commit_hash'  => '8afc0e3ba',
-    'deployed_at'  => '2026-05-26 12:10:48',
+    'commit_title' => 'logo a dx mobile',
+    'commit_hash'  => '0698fed3c',
+    'deployed_at'  => '2026-05-26 12:26:49',
 ];
