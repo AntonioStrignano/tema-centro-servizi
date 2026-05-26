dc9f0ab537653278baca141f7d5c3eff22bda020
Antonio Strignano <a.strignano@yahoo.it>
2026-05-26 11:47:46 +0200

    fix spazio binco overflow sotto layout pagina archivi

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Migra i componenti homepage da stitch-homepage.css a site.css e disattiva il caricamento di stitch-homepage.css come placeholder temporaneo',
-    'commit_hash'  => '9e262f633',
-    'deployed_at'  => '2026-05-26 11:43:02',
+    'commit_title' => 'fix spazio binco overflow sotto layout pagina archivi',
+    'commit_hash'  => 'a19a7b3a4',
+    'deployed_at'  => '2026-05-26 11:47:46',
 ];
