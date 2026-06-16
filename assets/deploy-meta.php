2ba42387021c819f1e227b125b22c6c150fcb75d
Antonio Strignano <a.strignano@yahoo.it>
2026-06-16 10:50:59 +0200

    Aggiunge uno stile di separazione tra gli elementi della pagina admin 'Attività' per migliorare la leggibilità e l'organizzazione visiva.

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Migliora la pagina admin 'Attività': aggiorna il layout delle sezioni con un design più moderno, aggiunge pulsanti per gestire le azioni delle sezioni e ottimizza l'interazione con le sezioni espandibili.',
-    'commit_hash'  => '0f4297877',
-    'deployed_at'  => '2026-06-16 10:49:50',
+    'commit_title' => 'Aggiunge uno stile di separazione tra gli elementi della pagina admin 'Attività' per migliorare la leggibilità e l'organizzazione visiva.',
+    'commit_hash'  => 'ecde14b53',
+    'deployed_at'  => '2026-06-16 10:50:59',
 ];
