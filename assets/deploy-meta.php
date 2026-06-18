09f4f3a942c30ab2a7c203680b10b943b668c0ab
Antonio Strignano <a.strignano@yahoo.it>
2026-06-18 10:13:28 +0200

    Aggiorna la gestione dei pulsanti di navigazione nella lightbox con miglioramenti di z-index e visibilità

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Rimuove la gestione manuale dei pulsanti di navigazione nella lightbox e migliora l'accessibilità con stili CSS aggiornati',
-    'commit_hash'  => '234ed8005',
-    'deployed_at'  => '2026-06-18 10:12:03',
+    'commit_title' => 'Aggiorna la gestione dei pulsanti di navigazione nella lightbox con miglioramenti di z-index e visibilità',
+    'commit_hash'  => '45a79d822',
+    'deployed_at'  => '2026-06-18 10:13:28',
 ];
