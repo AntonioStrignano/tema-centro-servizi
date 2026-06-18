a24e039530465d3c4d4e867101fd94c7e14bd066
Antonio Strignano <a.strignano@yahoo.it>
2026-06-18 11:26:43 +0200

    Aggiunge stili per migliorare la larghezza e il padding della sezione di ricerca

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Aggiunge stili per limitare la larghezza dell'intestazione e migliora il layout del contenuto nella pagina di ricerca',
-    'commit_hash'  => 'bbf466ac4',
-    'deployed_at'  => '2026-06-18 11:23:54',
+    'commit_title' => 'Aggiunge stili per migliorare la larghezza e il padding della sezione di ricerca',
+    'commit_hash'  => '29d6975ad',
+    'deployed_at'  => '2026-06-18 11:26:43',
 ];
