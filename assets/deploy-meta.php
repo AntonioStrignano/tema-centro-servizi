8624e4d131c182e2f3fbed23ed660d2ab24601cc
Antonio Strignano <a.strignano@yahoo.it>
2026-06-18 09:59:00 +0200

    Migliora lo stile della lightbox con nuove regole CSS e ottimizza la gestione della visualizzazione dei pulsanti di navigazione

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Aggiunge il supporto per la lightbox nella pagina 'Attività' e modifica il testo alternativo delle immagini',
-    'commit_hash'  => '40fb48bd5',
-    'deployed_at'  => '2026-06-18 09:56:42',
+    'commit_title' => 'Migliora lo stile della lightbox con nuove regole CSS e ottimizza la gestione della visualizzazione dei pulsanti di navigazione',
+    'commit_hash'  => 'e056bf911',
+    'deployed_at'  => '2026-06-18 09:59:00',
 ];
