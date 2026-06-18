2c2b89f1a419c04952220d9552e8e172c91ed446
Antonio Strignano <a.strignano@yahoo.it>
2026-06-18 11:39:39 +0200

    Aggiorna i colori dei link nel footer e nella testata per migliorare la leggibilità

--- assets/deploy-meta.php
+++ assets/deploy-meta.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'channel'      => 'git-push',
-    'commit_title' => 'Aggiorna il colore del testo nel pulsante di chiamata all'azione nella testata',
-    'commit_hash'  => '2176b796b',
-    'deployed_at'  => '2026-06-18 11:37:15',
+    'commit_title' => 'Aggiorna i colori dei link nel footer e nella testata per migliorare la leggibilità',
+    'commit_hash'  => '8f9a5c702',
+    'deployed_at'  => '2026-06-18 11:39:39',
 ];
